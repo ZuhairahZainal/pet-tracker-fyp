0d420f9e148703db9a3943f70b48e8ce5a47e35d
Zuhairah Zainal <19FTT1152@student.pb.edu.bn>
2021-08-27 23:10:27 +0800

    creating login page - jiqah

--- src/app/auth/login/login.page.ts
+++ src/app/auth/login/login.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoadingController, ToastController } from '@ionic/angular';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  email: string;
+  password: string;
+
+  constructor(
+    private router: Router,
+    private auth: AuthService,
+    private toastr: ToastController,
+    private loadingCtrl: LoadingController
+
+  ) { }
 
   ngOnInit() {
   }
+
+  register()
+  {
+    this.router.navigate(['/register']);
+  } //end of register
+
+  forgot()
+  {
+    this.router.navigate(['/forgot-password']);
+  } //end of forgot-password
+
+  guideline()
+  {
+    this.router.navigate(['/guideline']);
+  } //end of guideline
+
+  async login()
+  {
+    if(this.email && this.password)
+    {
+      const loading = await this.loadingCtrl.create({
+        message: 'Logging in..',
+        spinner: 'crescent',
+        showBackdrop: true
+      });
+
+      loading.present();
+
+      this.auth.login(this.email, this.password)
+      .then(()=> {
+        loading.dismiss();
+        this.router.navigate(['/home']);
+      })
+      .catch((error)=> {
+        loading.dismiss();
+        this.toast(error.message, 'danger');
+      });
+    } else {
+      this.toast('Please enter your email and password!', 'danger');
+    }
+  } //end of login
+
+  async toast(message, status)
+  {
+    const toast = await this.toastr.create({
+      // eslint-disable-next-line object-shorthand
+      message:message,
+      position: 'top',
+      color: status,
+      duration: 2000
+    });
+    toast.present();
+  } //end of toast
 
 }
