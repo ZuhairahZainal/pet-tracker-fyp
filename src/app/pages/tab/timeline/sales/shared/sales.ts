7804edca844cc54994e0ac0b9344ca9fcf4e492a
Zuhairah Zainal <19FTT1152@student.pb.edu.bn>
2021-08-27 23:36:53 +0800

    product model for new product to db  -aira

--- src/app/pages/tab/timeline/sales/shared/sales.ts
+++ src/app/pages/tab/timeline/sales/shared/sales.ts
@@ -1,2 +1,8 @@
-export interface Sale {
+export class NewProduct {
+  $key: string;
+  productName: string;
+  productCategory: string;
+  productPrice: number;
+  productDescription: string;
+  productImage: null;
 }
