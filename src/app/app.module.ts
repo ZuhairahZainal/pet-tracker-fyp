ab0868a78f5cbca13ee3cb054c345bb47d87f59b
Zuhairah Zainal <19FTT1152@student.pb.edu.bn>
2021-08-27 20:41:06 +0800

    import angular/firebase& form module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,32 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+
+// Firebase
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { environment } from 'src/environments/environment';
+
+// forms
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule,
+            AngularFireModule.initializeApp(environment.firebaseConfig),
+            AngularFireAuthModule,
+            AngularFireDatabaseModule,
+            AngularFireStorageModule,
+            AngularFirestoreModule,
+            FormsModule,
+            ReactiveFormsModule,
+            IonicModule.forRoot(),
+            AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
