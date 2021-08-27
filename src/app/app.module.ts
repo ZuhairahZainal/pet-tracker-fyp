c4efbde0c6c6520ebf493e319704559346b68827
Zuhairah Zainal <19FTT1152@student.pb.edu.bn>
2021-08-27 22:39:35 +0800

    import firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,16 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 
-// Firebase
-import { environment } from 'src/environments/environment';
+//firebase
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+
+
+//env
+import { environment } from '../environments/environment';
 
 // forms
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -19,11 +27,11 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule,
-            // AngularFireModule.initializeApp(environment.firebaseConfig),
-            // AngularFireAuthModule,
-            // AngularFireDatabaseModule,
-            // AngularFireStorageModule,
-            // AngularFirestoreModule,
+            AngularFireModule.initializeApp(environment.firebaseConfig),
+            AngularFireAuthModule,
+            AngularFireDatabaseModule,
+            AngularFireStorageModule,
+            AngularFirestoreModule,
             FormsModule,
             ReactiveFormsModule,
             IonicModule.forRoot(),
