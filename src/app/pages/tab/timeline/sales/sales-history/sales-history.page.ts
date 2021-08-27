9a72679b8c7ee039be030ebb528a22ec021cfc74
Zuhairah Zainal <19FTT1152@student.pb.edu.bn>
2021-08-27 23:38:32 +0800

    creating summaryof owners  sales post -aira

--- src/app/pages/tab/timeline/sales/sales-history/sales-history.page.ts
+++ src/app/pages/tab/timeline/sales/sales-history/sales-history.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NewProduct } from '../shared/sales';
+import { ProductService } from './../shared/product.service';
 
 @Component({
   selector: 'app-sales-history',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SalesHistoryPage implements OnInit {
 
-  constructor() { }
+  Products: NewProduct[] = [];
+
+  constructor(
+    private productService: ProductService
+  ) { }
 
   ngOnInit() {
+    this.fetchProducts();
+    let productRes = this.productService.getProductList();
+    productRes.snapshotChanges().subscribe(res => {
+      this.Products = [];
+      res.forEach(item => {
+        let a = item.payload.toJSON();
+        a['$key'] = item.key;
+        this.Products.push(a as NewProduct);
+      })
+    })
+  }
+
+  fetchProducts() {
+    this.productService.getProductList().valueChanges().subscribe(res => {
+      console.log(res)
+    })
+  }
+
+  deleteProduct(productId) {
+    console.log(productId)
+    if (window.confirm('Do you really want to delete?')) {
+      this.productService.deleteProduct(productId)
+    }
   }
 
 }
