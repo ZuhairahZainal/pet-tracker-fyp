176c68b7b956e887ced25efdef8718213da9b126
Zuhairah Zainal <19FTT1152@student.pb.edu.bn>
2021-08-27 23:36:30 +0800

    creatinf cart user interface  - aira

--- src/app/pages/tab/timeline/sales/cart/cart.page.ts
+++ src/app/pages/tab/timeline/sales/cart/cart.page.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { BehaviorSubject } from 'rxjs';
+import { ProductService } from '../shared/product.service';
+import { NewProduct } from '../shared/sales';
+import { CartService } from './shared/cart.service';
 
 @Component({
   selector: 'app-cart',
@@ -7,9 +12,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartPage implements OnInit {
 
-  constructor() { }
+  cart = [];
+  Products: NewProduct[] = [];
+  cartItemCount: BehaviorSubject<number>;
+
+  @ViewChild('cart', {static: false, read: ElementRef})fab: ElementRef;
+
+  constructor(private cartService: CartService,
+              private modalCtrl: ModalController,
+              private productService: ProductService) {}
 
   ngOnInit() {
+    this.cart = this.cartService.getCart();
+    this.cartItemCount = this.cartService.getCartItemCount();
+
+    this.fetchProducts();
+    let productRes = this.productService.getProductList();
+    productRes.snapshotChanges().subscribe(res => {
+      this.Products = [];
+      this.Products = this.cartService.getProducts();
+      res.forEach(item => {
+        let a = item.payload.toJSON();
+        a['$key'] = item.key;
+        this.Products.push(a as NewProduct);
+      })
+    })
+
+    this.cart = this.cartService.getCart();
+    this.cartItemCount = this.cartService.getCartItemCount();
   }
 
+  fetchProducts() {
+    this.productService.getProductList().valueChanges().subscribe(res => {
+      console.log(res)
+    })
+  }
+
+  removeCartItem(product) {
+    this.cartService.removeProduct(product);
+  }
+
+  getTotal() {
+    return this.cart.reduce((i, j) => i + j.productPrice, 0);
+  }
 }
