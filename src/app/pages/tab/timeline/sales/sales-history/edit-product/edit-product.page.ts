2514afc1603b3df59fa2e6b653bc84be78d3e046
Zuhairah Zainal <19FTT1152@student.pb.edu.bn>
2021-08-27 23:41:00 +0800

    Creating pagefor editing existing product -aira

--- src/app/pages/tab/timeline/sales/sales-history/edit-product/edit-product.page.ts
+++ src/app/pages/tab/timeline/sales/sales-history/edit-product/edit-product.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from "@angular/router";
+import { FormGroup, FormBuilder } from "@angular/forms";
+import { ProductService } from './../../shared/product.service';
 
 @Component({
   selector: 'app-edit-product',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditProductPage implements OnInit {
 
-  constructor() { }
+  updateProductForm: FormGroup;
+  productId: any;
+
+  constructor(private pdtService: ProductService,
+    private actRoute: ActivatedRoute,
+    private router: Router,
+    public fb: FormBuilder) {
+      this.productId = this.actRoute.snapshot.paramMap.get('productId');
+      this.pdtService.getProduct(this.productId).valueChanges().subscribe(res => {
+      this.updateProductForm.setValue(res);
+    });
+  }
+
 
   ngOnInit() {
+    this.updateProductForm = this.fb.group({
+      productName: [''],
+      productCategory: [''],
+      productPrice: [''],
+      productDescription: [''],
+      productImage: ['']
+    })
+    console.log(this.updateProductForm.value)
+  }
+
+  //update form for changes
+  updateForm() {
+    this.pdtService.updateProduct(this.productId, this.updateProductForm.value)
+      .then(() => {
+        this.router.navigate(['tab/timeline/sales/sales-history']);
+      })
+      .catch(error => console.log(error));
   }
 
 }
+
