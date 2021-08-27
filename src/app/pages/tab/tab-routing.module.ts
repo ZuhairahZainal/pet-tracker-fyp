5a7fc5e15dc715829c3e438014ddd3b2adec7320
Zuhairah Zainal <19FTT1152@student.pb.edu.bn>
2021-08-28 01:21:07 +0800

    configuration of bottom tab

--- src/app/pages/tab/tab-routing.module.ts
+++ src/app/pages/tab/tab-routing.module.ts
@@ -6,27 +6,29 @@ import { TabPage } from './tab.page';
 const routes: Routes = [
   {
     path: '',
-    component: TabPage
-  },
-  {
-    path: 'timeline',
-    loadChildren: () => import('./timeline/timeline.module').then( m => m.TimelinePageModule)
-  },
-  {
-    path: 'adoption',
-    loadChildren: () => import('./adoption/adoption.module').then( m => m.AdoptionPageModule)
-  },
-  {
-    path: 'online-chat',
-    loadChildren: () => import('./online-chat/online-chat.module').then( m => m.OnlineChatPageModule)
-  },
-  {
-    path: 'user-profile',
-    loadChildren: () => import('./user-profile/user-profile.module').then( m => m.UserProfilePageModule)
-  },
-  {
-    path: 'location-tracker',
-    loadChildren: () => import('./location-tracker/location-tracker.module').then( m => m.LocationTrackerPageModule)
+    component: TabPage,
+    children:[
+      {
+        path: 'timeline',
+        loadChildren: () => import('./timeline/timeline.module').then( m => m.TimelinePageModule)
+      },
+      {
+        path: 'adoption',
+        loadChildren: () => import('./adoption/adoption.module').then( m => m.AdoptionPageModule)
+      },
+      {
+        path: 'user-profile',
+        loadChildren: () => import('./user-profile/user-profile.module').then( m => m.UserProfilePageModule)
+      },
+      {
+        path: 'location-tracker',
+        loadChildren: () => import('./location-tracker/location-tracker.module').then( m => m.LocationTrackerPageModule)
+      },
+      {
+        path: 'online-chat',
+        loadChildren: () => import('./online-chat/online-chat.module').then( m => m.OnlineChatPageModule)
+      }
+    ]
   }
 ];
 
