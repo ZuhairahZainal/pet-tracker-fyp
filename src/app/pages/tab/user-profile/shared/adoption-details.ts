e0645282b96ee9e04ec80e8bbf77277105b5b518
Zuhairah Zainal <19FTT1152@student.pb.edu.bn>
2021-09-24 00:04:34 +0800

    add adoption interface

---
+++ src/app/pages/tab/user-profile/shared/adoption-details.ts
@@ -0,0 +1,12 @@
+export interface AdoptionDetails {
+  petName: string;
+  petAge: number;
+  petBreed: string;
+  petCategory: string;
+  petCondition: string;
+  petDescription: string;
+  petGender: string;
+  petSpayStatus: string;
+  petImage: null;
+  petMedicalRecord: null;
+}
