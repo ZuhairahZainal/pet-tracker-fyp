10aba05277d19c886c008711f064fd4456c82f95
Zuhairah Zainal <19FTT1152@student.pb.edu.bn>
2021-10-12 00:11:02 +0800

    add plugin call, splashscreen, geos and status bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,13 +20,17 @@ import { environment } from '../environments/environment';
 //forms
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { Ng2SearchPipeModule  } from 'ng2-search-filter';
-
 //services
 import { AuthService } from './services/auth/auth.service';
 
 //guards
 import { AuthGuard } from '../app/guards/auth.guard';
+
+// plugins
+import { CallNumber } from '@ionic-native/call-number/ngx';
+import { SplashScreen} from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Ng2SearchPipeModule  } from 'ng2-search-filter';
 
 @NgModule({
   declarations: [AppComponent],
@@ -42,7 +46,7 @@ import { AuthGuard } from '../app/guards/auth.guard';
             Ng2SearchPipeModule,
             IonicModule.forRoot(),
             AppRoutingModule],
-  providers: [AuthService, AuthGuard, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [AuthService, SplashScreen, StatusBar, CallNumber, AuthGuard, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/pages/tab/timeline/vet/vet-detail/vet-detail.page.ts
+++ src/app/pages/tab/timeline/vet/vet-detail/vet-detail.page.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Vet } from 'src/app/models/vet/vet';
 import { VetService } from 'src/app/services/vet/vet.service';
 import firebase from 'firebase/app';
+import { CallNumber } from '@ionic-native/call-number/ngx';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-vet-detail',
@@ -16,7 +18,10 @@ export class VetDetailPage implements OnInit {
   userId: string;
 
   constructor(private vetService: VetService,
-              private activatedRoute: ActivatedRoute) {}
+              private activatedRoute: ActivatedRoute,
+              private alertCtrl: AlertController,
+              private router: Router,
+              private callNumber: CallNumber) {}
 
 
   ngOnInit() {
@@ -33,6 +38,32 @@ export class VetDetailPage implements OnInit {
   getUserId(){
     let user = firebase.auth().currentUser;
     this.userId = user.uid;
+  }
+
+  async callVet(n: number, vet:string){
+    const alert = await this.alertCtrl.create({
+      header: `Call ${vet}`,
+      message: `Do you want to contact ${vet}?`,
+      buttons: [
+        {
+          text: 'Yes',
+          handler: () => this.launchDialer(n),
+        },
+        {
+          text: 'No',
+        },
+      ],
+    });
+
+    alert.present();
+
+  }
+
+
+  launchDialer(n: number){
+    this.callNumber.callNumber(n.toString(), true)
+    .then(() => console.log('Launched dialer!'))
+    .catch(() => console.log('Error launching dialer'));
   }
 
 }
